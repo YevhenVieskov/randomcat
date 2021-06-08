c4d09f233845d71558495bef409678c597a62ba5
Yevhen Vieskov <evveskov@gmail.com>
2021-06-08 12:49:28 +0300

    unit test sh 5

--- tests/runUT.sh
+++ tests/runUT.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-PATH=$WORKSPACE/venv/bin:$HOME/.local/bin:$PATH
-if [ ! -d "venv" ] then;
-	#pip3 install virtualenv --user
-	#virtualenv venv
-	pip3 install --user virtualenv
-    python3 -m venv testenv	
+PATH=$WORKSPACE/testenv/bin:$HOME/.local/bin:$PATH
+if [ ! -d "testenv" ]; then
+  pip3 install --user virtualenv
+  python3 -m venv testenv
 fi
+
 source testenv/bin/activate
 pip3 install --no-cache-dir -r ./requirements.txt
 python3  ./test_flask_app.py
-#deactivate
-#exit
-
+deactivate
+exit
